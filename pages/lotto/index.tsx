c8917d600e0f11b534a94905f76dc98c5954bc07
bluelovers <codelovers@users.sourceforge.net>
2020-08-01 22:23:00 +0800

    feat: use @lazy-react/react-suspense

--- pages/lotto/index.tsx
+++ pages/lotto/index.tsx
@@ -1,27 +1,50 @@
-import React, { PropsWithChildren, ReactNode } from 'react';
+import React, { PropsWithChildren, ReactNode, Suspense } from 'react';
 import { GetStaticProps } from 'next';
-import useSwr from 'swr'
+import useSwr, { mutate } from 'swr'
 import LottoItem from '../../components/lotto/LottoItem';
 import LottoList from '../../components/lotto/LottoList';
 import Layout from '../../components/Layout';
+import styles from './index.module.css'
+import ReactSuspense from '@lazy-react/react-suspense';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
+const isServer = typeof window === 'undefined'
+
+const ShowLotto = () =>
+{
+	const { data, error } = useSwr('/api/lotto', fetcher, {
+		suspense: true,
+	});
+
+	if (error) return <div>Failed to load users</div>
+
+	return <>
+		<div className={styles.area}>
+			<LottoList list={data.list_pick}></LottoList>
+		</div>
+		<div className={styles.area}>
+			<button onClick={() => mutate('/api/lotto')}>reload</button>
+		</div>
+		<div className={styles.area}>
+			<pre>{JSON.stringify(data, null, 2)}</pre>
+		</div>
+	</>
+}
+
+const Loading = () => <div>loading...</div>;
 
 export default (prop: PropsWithChildren<{
 	env: any
 }>) =>
 {
-	const { data, error } = useSwr('/api/lotto', fetcher);
-
-	if (error) return <div>Failed to load users</div>
-	if (!data) return <div>Loading...</div>
-
 	return <>
 		<Layout
 			title={`Lotto`}
 		>
-		<LottoList list={data.list_pick}></LottoList>
-		<pre>{JSON.stringify(data, null, 2)}</pre>
+			<ReactSuspense fallback={<Loading />}>
+				<ShowLotto />
+			</ReactSuspense>
 		</Layout>
 	</>
 }
